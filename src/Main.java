8e98c72a8a10b2e220336fc60816420a008c5267
TatGold <ta-nit@yandex.ru>
2023-01-22 10:25:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 10_000;
+        int mile = 20;
+
+        int bonus = cost / mile;
+
+        System.out.println("Количество бонусных миль: " + bonus);
+
+    }
+}
